59069a95c3c55549fd83fa8419e837c2aa729c8d
Corey Smedstad <smedstadc@gmail.com>
2015-04-20 03:18:09 -0400

    fix .time to report 24 hour format like it used to

--- plugins/time_plugin.py
+++ plugins/time_plugin.py
@@ -2,14 +2,13 @@
 
 from datetime import datetime
 
-
 def init_plugin(command_map):
     command_map.map_command(".time", utc_time)
     command_map.map_command(".upladtime", uplad_time)
 
 
 def utc_time():
-    return [datetime.utcnow().strftime("%A, %d. %B %Y %I:%M%p UTC")]
+    return [datetime.utcnow().strftime("%A, %d. %B %Y %H:%M UTC")]
 
 
 def uplad_time():
